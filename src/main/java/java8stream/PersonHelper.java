3d04ada47a149c04b86387cafca7d8c1f909c6d6
ksola <ksola@github.com>
2015-05-23 20:47:37 +0200

    exercieses

--- src/main/java/java8stream/PersonHelper.java
+++ src/main/java/java8stream/PersonHelper.java
@@ -1,36 +1,29 @@
 package java8stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class PersonHelper {
 
-	static Comparator<? super Person> ageComparator = (p1, p2) -> (p1.getAge() > p2.getAge()) ? 1 : ((p1
-			.getAge() < p2.getAge()) ? -1 : 0);
-	
+
 	public static Optional<Person> getOldestPerson(List<Person> persons) {
-		return persons.stream().max(ageComparator);
+		return null;
 	}
 
 	public static List<Person> getThreeYoungestPersons(List<Person> persons) {
-		return persons
-				.stream()
-				.sorted(ageComparator).limit(3)
-				.collect(Collectors.toList());
+		return null;
 	}
 	
 	
 	public static String concatenateNamesCommaSeparated(List<Person> persons) {
-		return persons.stream().map(Person::getName).collect(Collectors.joining(", "));
+		return null;
 	}
 
 	public static int getAgeSum(List<Person> persons) {
-		return persons.stream().map(Person::getAge).reduce(0, (age, sum) -> sum + age);
+		return 0;
 	}
 	
 	public static int getAgeSumOfGroupedPersons(List<List<Person>> groupedPersons) {
-		return groupedPersons.stream().map(list -> list.stream().map(Person::getAge).reduce(0, Integer::sum)).reduce(0, Integer::sum);
+		return 0;
 	}
 }
